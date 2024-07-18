b8b7ca01a429beaba4c9e0e982e8fea58764a4cd
itay mendelawy <itay.mendelawy@gmail.com>
2024-07-18 13:08:53 +0100

    without any libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import '@frontend/stencil.examples.my-stencil-comp';
+import { wrapWc } from 'wc-react';
 import './App.css';
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'my-stencil-comp': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+
 function App() {
+  const MyStencilComponent = wrapWc('my-stencil-comp');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <my-stencil-comp></my-stencil-comp>
         </p>
         <a
           className="App-link"
